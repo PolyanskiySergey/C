52fd16e5dfee1803c35f285c4db9a307a63e3bd1
PolyanskiySergey <151769523+PolyanskiySergey@users.noreply.github.com>
2023-12-20 13:08:21 +0300

    Программы на языке C

---
+++ Операции с векторами/Vector/Vector.cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+#include<math.h>
+
+using namespace std;
+struct vector
+{
+    double x, y, z;
+    vector(double X, double Y, double Z)
+    {
+        x = X;
+        y = Y;
+        z = Z;
+    };
+
+    double length(void)
+    {
+        return sqrt(x * x + y * y + z * z);
+    };
+};
+void addition (vector v1, vector v2, vector S)
+{
+    S.x = v1.x + v2.x;
+    S.y = v1.y + v2.y;
+    S.z = v1.z + v2.z;
+    cout << "Сумма векторов A+B=" << S.x << " " << S.y << " " << S.z << "\n";
+};
+
+void difference (vector v1, vector v2, vector D)
+{
+    D.x = v1.x - v2.x;
+    D.y = v1.y - v2.y;
+    D.z = v1.z - v2.z;
+    cout << "Разность векторов A-B=" << D.x << " " << D.y << " " << D.z << "\n";
+};
+
+void main(void)
+{
+    setlocale(LC_ALL, "Russian");
+    double x1, x2, y1, y2, z1, z2;
+    cout << "Введите координаты вектора A=";
+    cin >> x1 >> y1 >> z1;
+    cout << "Введите координато выектора B=";
+    cin >> x2 >> y2 >> z2;
+    vector v(x1, y1, z1);
+    vector v1(x1, y1, z1);
+    vector v2(x2, y2, z2);
+    vector S(0, 0, 0);
+    vector D(0, 0, 0);
+    double l = v.length();
+    cout << "Длина вектора A=" << l << endl;
+    addition(v1, v2, S);
+    difference(v1, v2, D);
+}
---
+++ Операции с динамическими матрицами/DinamicMatrix/DinamicMatrix.cpp
@@ -0,0 +1,119 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+const int N = 3;
+int main()
+{
+	int** A = (int**)malloc(N * sizeof(int*));
+	int** B = (int**)malloc(N * sizeof(int*));
+	int** C = (int**)malloc(N * sizeof(int*));
+	int** D = (int**)malloc(N * sizeof(int*));
+	int** E = (int**)malloc(N * sizeof(int*));
+
+	for (int i = 0; i < N; i++)
+	{
+		A[i] = (int*)malloc(N * sizeof(int));
+		B[i] = (int*)malloc(N * sizeof(int));
+		C[i] = (int*)malloc(N * sizeof(int));
+		D[i] = (int*)malloc(N * sizeof(int));
+		E[i] = (int*)malloc(N * sizeof(int));
+	}
+
+	srand(time(NULL));
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+		{
+			A[i][j] = rand() % 10;
+			B[i][j] = rand() % 10;
+		}
+printf("A:\n");
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+			printf("%d ", A[i][j]);
+		printf("\n");
+	}
+	printf("\nB:\n");
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+			printf("%d ", B[i][j]);
+		printf("\n");
+	}
+	
+	printf("Choose operation:\n");
+	char x;
+	scanf_s("%c", &x);
+	switch (x) {
+	case '+':
+		for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+		{
+			D[i][j] = 0;
+			D[i][j] += A[i][j] + B[i][j];
+		}
+printf("\nA+B:\n");
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+			printf("%d ", D[i][j]);
+		printf("\n");
+	}
+		break;
+	case '-':
+		for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+		{
+			E[i][j] = 0;
+			E[i][j] += A[i][j] - B[i][j];
+		}
+printf("\nA-B:\n");
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+			printf("%d ", E[i][j]);
+		printf("\n");
+	}
+		break;
+	case '*':
+		for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+		{
+			C[i][j] = 0;
+			for (int k = 0; k < N; k++)
+				C[i][j] += A[i][k] * B[k][j];
+		}
+printf("\nAxB:\n");
+	for (int i = 0; i < N; i++)
+	  {
+		for (int j = 0; j < N; j++)
+			printf("%d ", C[i][j]);
+		printf("\n");
+	  }
+		break;
+	    case 'D':
+		 printf("\nDet A:\n");
+	     int det = A[0][0] * A[1][1] * A[2][2] - A[0][0] * A[1][2] * A[2][1] - A[0][1] * A[1][0] * A[2][2] + A[0][1] * A[2][0] * A[1][2] + A[0][2] * A[1][0] * A[2][1] - A[0][2] * A[1][1] * A[2][0];
+	     printf("%d", det);
+	     printf("\n");
+		break;
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		free(A[i]);
+		free(B[i]);
+		free(C[i]);
+		free(D[i]);
+		free(E[i]);
+	}
+	free(A);
+	free(B);
+	free(C);
+	free(D);
+	free(E);
+	return 0;
+
+}
